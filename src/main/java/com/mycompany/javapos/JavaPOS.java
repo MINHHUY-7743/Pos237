130e9a571fb2aabccbcd7f3a19bde86488d6c866
MINHHUY-7743 <nguyenminhhuy3756@gmail.com>
2024-11-16 01:26:23 +0700

    pos

---
+++ src/main/java/com/mycompany/javapos/JavaPOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javapos;
+
+/**
+ *
+ * @author Huy
+ */
+public class JavaPOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
